d89ffd7254e2ae2dbc212a360ca3a2abc3c3e822
giovanni <144000giovanni@gmail.com>
2024-05-15 04:44:45 +1200

    'update'

--- channels/lineage_journey/stream_video_to_macbookpro_server.py
+++ channels/lineage_journey/stream_video_to_macbookpro_server.py
@@ -3,7 +3,7 @@ from create_playlist_json import shuffle
 import json
 
 jsonfiles = shuffle()
-port = "1935"
+port = "1940"
 def getJsonObj(filename):
   fileopen = open(filename, "r", encoding="utf-8")
   jsonobj = json.loads(fileopen.read())
@@ -55,15 +55,15 @@ for filename in jsonfiles:
   try:
     video_id = jsonobj["id"]
 
-    #proceed = input("continue: ")
-    #if proceed=="y":
+    proceed = input("continue: ")
+    if proceed=="y":
     #command = 'ffmpeg -re -i ' +  '"'+url+'"' + ' -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2  -preset fast -b:v 5M -maxrate 6M -bufsize 3M -threads 4 -f flv rtmp://192.168.1.17:1935/show/stream'
-    updateJsonObj(video_id)
-    jsonobj = getJsonObj(filename)
+      updateJsonObj(video_id)
+      jsonobj = getJsonObj(filename)
     #stream(jsonobj)
-    streamToMacbookProServer(jsonobj)
-    #else:
-    #  break    
+      streamToMacbookProServer(jsonobj)
+    else:
+      break    
   except Exception as e:
     print("error: " + str(e))
 #command = 'ffmpeg -re -i "https://rr5---sn-j5aa2a0n-53ae.googlevideo.com/videoplayback?expire=1715467909&ei=JaI_ZpOXJN-k9fwPkMyAoAw&ip=2405%3Ada40%3A1195%3Ac000%3Ad1ba%3A432d%3A5169%3Ace75&id=o-ADNDuAQ0gjS4njsv30hGE7ds_c5-MY-B2OHrf2itxZUB&itag=136&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=in&mm=31%2C26&mn=sn-j5aa2a0n-53ae%2Csn-npoe7nes&ms=au%2Conr&mv=m&mvi=5&pl=41&initcwndbps=2380000&vprv=1&svpuc=1&mime=video%2Fmp4&rqh=1&gir=yes&clen=1030051589&dur=7045.550&lmt=1712416685506368&mt=1715445894&fvip=2&keepalive=yes&c=IOS&txp=6219224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhANgbD96KkZY1u_Pu7YvCtU6-iKBzD2eo4ReWdFsx5qogAiAFDb4I98OVhEFYaijqV00A5P8J8II_7EggI79svZSr_A%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AHWaYeowRAIgYZa8yJYCWHl5S_9-XAIa40TiYHMPE_eRDc2V4mEl5VECIDMNBVIGM3OWgXua4QMPmBXWYSgURTmIFI2XLMP8W-o2" -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://192.168.1.17:1935/show/stream'
